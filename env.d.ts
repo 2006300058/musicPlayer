dc8400708695769e4a6dfd7c06c7e4e8e9cba0e4
zyf666 <3042760692@qq.com>
2023-03-06 21:33:28 +0800

    初始化

---
+++ env.d.ts
@@ -0,0 +1,2 @@
+//代码书写规则配置文件
+///<reference types="vite/client" />
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { resolve } from 'path'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import { fileURLToPath } from 'url'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers:[ElementPlusResolver()]
+    })
+    
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'docs',
+  },
+  server: {
+    port: 3033
+  }
+  
+
+})
